045c211dfe0f9b37a3dc06049aaa19687fa835d7
Svatopluk Ordelt <svata@ordelt.cz>
2017-07-08 09:44:11 +0200

    just refactoring

--- src/Test.java
+++ src/Test.java
@@ -1,64 +1,59 @@
-import freemarker.template.*;
-import java.util.*;
-
-import cz.rubysoft.hcch.KenticoTransformer;
-
-import java.io.*;
+import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
+import cz.rubysoft.hcch.HcchTemplate;
+import cz.rubysoft.hcch.KenticoTransformer;
+
 public class Test {
-	static String readFile(String path, Charset encoding) 
-			  throws IOException 
-			{
-			  byte[] encoded = Files.readAllBytes(Paths.get(path));
-			  return new String(encoded, encoding);
-			}
-	
+    static String readFile(String path, Charset encoding) throws IOException {
+        byte[] encoded = Files.readAllBytes(Paths.get(path));
+        return new String(encoded, encoding);
+    }
 
     public static void main(String[] args) throws Exception {
 
         /* ------------------------------------------------------------------------ */
-        /* You should do this ONLY ONCE in the whole application life-cycle:        */
+        /* You should do this ONLY ONCE in the whole application life-cycle: */
 
         /* Create and adjust the configuration singleton */
-//        Configuration cfg = new Configuration(Configuration.VERSION_2_3_25);
-//        cfg.setDirectoryForTemplateLoading(new File("./templates"));
-//        cfg.setDefaultEncoding("UTF-8");
-//        cfg.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);
-//        cfg.setLogTemplateExceptions(false);
+        // Configuration cfg = new Configuration(Configuration.VERSION_2_3_25);
+        // cfg.setDirectoryForTemplateLoading(new File("./templates"));
+        // cfg.setDefaultEncoding("UTF-8");
+        // cfg.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);
+        // cfg.setLogTemplateExceptions(false);
 
         /* ------------------------------------------------------------------------ */
-        /* You usually do these for MULTIPLE TIMES in the application life-cycle:   */
+        /* You usually do these for MULTIPLE TIMES in the application life-cycle: */
 
         /* Create a data-model */
-//        Map root = new HashMap();
-//        
-//        
-//       
-//        
-//        root.put("user", "Big Joe");
-//        Product latest = new Product();
-//        latest.setUrl("products/greenmouse.html");
-//        latest.setName("green mouse");
-//        root.put("latestProduct", latest);
+        // Map root = new HashMap();
+        //
+        //
+        //
+        //
+        // root.put("user", "Big Joe");
+        // Product latest = new Product();
+        // latest.setUrl("products/greenmouse.html");
+        // latest.setName("green mouse");
+        // root.put("latestProduct", latest);
 
         String fileAsString = readFile("kentico_message.json", StandardCharsets.UTF_8);
-        
-        String result = KenticoTransformer.theTransformer().transform(fileAsString, "message.ftl");
 
-        System.out.println(result);
+        HcchTemplate result = KenticoTransformer.theTransformer().transform(fileAsString, "message.ftl");
 
-//        root.put("jsonTest", fileAsString);
-//        /* Get the template (uses cache internally) */
-//        Template temp = cfg.getTemplate("test.ftl");
-//
-//        /* Merge data-model with template */
-//        Writer out = new OutputStreamWriter(System.out);
-//        temp.process(root, out);
-//        // Note: Depending on what `out` is, you may need to call `out.close()`.
-//        // This is usually the case for file output, but not for servlet output.
+        System.out.println(result.getTemplate());
+
+        // root.put("jsonTest", fileAsString);
+        // /* Get the template (uses cache internally) */
+        // Template temp = cfg.getTemplate("test.ftl");
+        //
+        // /* Merge data-model with template */
+        // Writer out = new OutputStreamWriter(System.out);
+        // temp.process(root, out);
+        // // Note: Depending on what `out` is, you may need to call `out.close()`.
+        // // This is usually the case for file output, but not for servlet output.
     }
 }
